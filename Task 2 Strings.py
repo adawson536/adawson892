b03804b3f232d3587c175671199cfff381e0853d
adawson536 <143815005+adawson536@users.noreply.github.com>
2026-08-18 21:13:16 -0400

    Update Task 2 Strings.py

--- Task 2 Strings.py
+++ Task 2 Strings.py
@@ -1,2 +1,3 @@
 Wii Sports' has a global sales percentage of 14.10 in the list of games.
 Pokemon has been extracted from its fun fact that it has red and blue colors.
+#1 Best Seller: Wii Sports (2006) - $82.74M global sales

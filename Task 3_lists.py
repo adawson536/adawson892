701839c5309c433eb23dc236397eba1621b16c1f
adawson536 <143815005+adawson536@users.noreply.github.com>
2026-08-19 12:15:08 -0400

    added Task 3a. game_names Task 3_lists.py

--- Task 3_lists.py
+++ Task 3_lists.py
@@ -3,3 +3,4 @@ messy names = wii sports
               tetris
               mario kart wii
 
+print(game_names) =['Wii Sports', 'Super Mario Bros.', 'Mario Kart Wii', 'Wii Sports Resort', 'Pokemon Red/Blue', 'Tetris', 'New Super Mario Bros.', 'Wii Play', 'New Super Mario Bros. Wii', 'Duck Hunt', 'Nintendogs', 'Mario Kart DS', 'Pokemon Gold/Silver', 'Wii Fit', 'Kinect Adventures!', 'Grand Theft Auto V', 'Grand Theft Auto: San Andreas', 'Super Mario World', 'Brain Age', 'Pokemon Diamond/Pearl']

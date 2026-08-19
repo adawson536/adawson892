23f481987cea40572b4c30442bae060e07badb5c
adawson536 <143815005+adawson536@users.noreply.github.com>
2026-08-18 21:14:08 -0400

    Removed a String Task 3_lists.py

--- Task 3_lists.py
+++ Task 3_lists.py
@@ -2,4 +2,4 @@ Add Task 3 - Lists and Aggregates
 messy names = wii sports
               tetris
               mario kart wii
-#1 Best Seller: [1, 'Wii Sports', 'Wii', 2006, 'Sports', 'Nintendo', 41.49, 29.02, 3.77, 82.74] (2006) - $82.74M global sales
+
